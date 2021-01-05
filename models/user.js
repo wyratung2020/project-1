4afc5cdd958dc7e06b4823a1be6c9d558833310f
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-01-05 14:14:10 +0700

    feat: add joindate to user detail

--- models/user.js
+++ models/user.js
@@ -3,30 +3,36 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
- firstname		: String,
- lastname       : String,
- username       : String,
- phone          : Number,
- email 			: String,
- password 		: String,
- roles          : String,
+    firstname: String,
+    lastname: String,
+    username: String,
+    phone: Number,
+    email: String,
+    password: String,
+    roles: String,
+    joindate: Date
 });
 
 // Các phương thức ======================
 // Tạo mã hóa mật khẩu
-userSchema.methods.encryptPassword = function(password){
+userSchema.methods.encryptPassword = function (password)
+{
     return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
 };
 
 // kiểm tra mật khẩu có trùng khớp
-userSchema.methods.validPassword = function(password){
+userSchema.methods.validPassword = function (password)
+{
     return bcrypt.compareSync(password, this.password);
 };
 
-userSchema.methods.isGroupAdmin = function(checkRole){
-    if(checkRole === "ADMIN"){
+userSchema.methods.isGroupAdmin = function (checkRole)
+{
+    if (checkRole === "ADMIN")
+    {
         return true;
-    } else {
+    } else
+    {
         return false;
 
     };
