8f1e2ce15fca169e16a7a8d4064b46f870d9b1c8
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-01-06 23:05:07 +0700

    add new fields to giohang

--- models/giohang.js
+++ models/giohang.js
@@ -10,7 +10,10 @@ var Cart = new Schema({
     thanhpho: String,
     cart: Object,
     st: Number,
+    danhan: Number,
     Tien: Number,
-    nguoidat: { type: Schema.Types.ObjectId, ref: 'User' }
+    nguoidat: { type: Schema.Types.ObjectId, ref: 'User' },
+    ngaydat: Date,
+    tongSoLuong: Number
 });
 module.exports = mongoose.model('Giohang', Cart);
